d256ef2cbc364286a8c17c484502c29f2d30faf6
Tuan Tran <tuan.tranleanh@anu.edu.au>
2021-06-30 10:23:36 +1000

    muli-pages

---
+++ Examples/06_multi_page_apps_using_tabsets.R
@@ -0,0 +1,28 @@
+## layouting - tabsets
+library(shiny)
+
+server <- function(input, output, session) {}
+
+ui <- fluidPage(
+  
+  titlePanel("using Tabsets"), # our title
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      sliderInput(inputId = "s1",
+                  label = "My Slider",
+                  value = 1, min = 1, max = 20)
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel(title="Tab1", "First Tab"),
+        tabPanel("Tab2", "Second Tab"),
+        tabPanel("Tab3", "Third Tab")
+      )
+    )
+  )
+)
+
+shinyApp(ui = ui, server = server)
